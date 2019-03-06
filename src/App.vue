3f7670ec20716fa7f5ba114f10b35a000d1b02f2
seashell113 <971866257@qq.com>
2019-03-07 00:55:48 +0800

    a little changes

--- src/App.vue
+++ src/App.vue
@@ -4,19 +4,19 @@
     <router-view />
     <mt-tabbar v-model="selected">
     <mt-tab-item id="home">
-      <img slot="icon" src="./assets/image/shouye.png">
+      <img @click="changeHash" slot="icon" src="./assets/image/shouye.png">
       首页
     </mt-tab-item>
     <mt-tab-item id="member">
-      <img slot="icon" src="./assets/image/wode.png">
+      <img @click="changeHash" slot="icon" src="./assets/image/wode.png">
       会员
     </mt-tab-item>
     <mt-tab-item id="shopcart">
-      <img slot="icon" src="./assets/image/gouwu.png">
+      <img @click="changeHash" slot="icon" src="./assets/image/gouwu.png">
       购物车
     </mt-tab-item>
     <mt-tab-item id="search">
-      <img slot="icon" src="./assets/image/lishi.png">
+      <img @click="changeHash" slot="icon" src="./assets/image/lishi.png">
       查找
     </mt-tab-item>
   </mt-tabbar>
@@ -25,18 +25,34 @@
 
 <script>
 export default {
-  data () {
+  data:function() {
     return {
       selected: ""
     }
   },
-  watch: {
-    selected(newV,oldV) {
-      console.log(newV);
-      this.$router.push({
-        name: newV
+  updated(){
+   
+      console.log(1);
+    
+  },
+  methods: {
+    changeHash(){
+        console.log(this.selected)
+      this.$nextTick(function (){
+        console.log(this.selected);
+        this.$router.push({
+          name: this.selected
+        })
       })
     }
+  },
+  watch: {
+   // selected(newV,oldV) {
+   //   console.log(newV);
+   //   this.$router.push({
+   //     name: newV
+   //   })
+   // }
   }
 }
 </script>
